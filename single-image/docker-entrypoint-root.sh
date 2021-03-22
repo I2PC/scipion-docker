76f344d36b49738e916d3ef109befe4a1a3084c3
ldelcano <ldelcano@gmail.com>
2021-03-22 11:01:50 +0100

    New single-image docker for Scipion

---
+++ single-image/docker-entrypoint-root.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+S_USER=scipionuser
+S_USER_HOME=/home/${S_USER}
+
+if [ -z "$ROOT_PASS" ] || [ -z "$USER_PASS" ] || [ -z "$USE_DISPLAY" ]; then
+	echo "please run the container with these variables: \nROOT_PASS\nUSER_PASS\nUSE_DISPLAY\n"
+	exit 1
+fi
+
+echo -e "$ROOT_PASS\n$ROOT_PASS" | passwd root
+echo -e "$USER_PASS\n$USER_PASS" | passwd $S_USER
+
+chown $S_USER:$S_USER $S_USER_HOME/scipion3/software/em
+
+mkdir ${S_USER_HOME}/ScipionUserData/data
+chown -R $S_USER:$S_USER $S_USER_HOME
+
+su -c ./docker-entrypoint.sh $S_USER
+
