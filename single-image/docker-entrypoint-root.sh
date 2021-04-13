037cc017636f9ac6b8b07eea32a9afb956664fd6
ldelcano <ldelcano@gmail.com>
2021-04-13 11:04:45 +0200

    Fixed problem with hostname in cryosparc

--- single-image/docker-entrypoint-root.sh
+++ single-image/docker-entrypoint-root.sh
@@ -10,7 +10,7 @@ chown -R $S_USER:$S_USER $S_USER_HOME/.config
 chown -R $S_USER:$S_USER $S_USER_HOME/ScipionUserData
 
 # Update cryosparc hostnames
-sed -i -e 's+CRYOSPARC_MASTER_HOSTNAME=.*+CRYOSPARC_MASTER_HOSTNAME="$HOSTNAME"+g' $S_USER_HOME/cryosparc3/cryosparc_master/config.sh
+sed -i -e "s+CRYOSPARC_MASTER_HOSTNAME=.*+CRYOSPARC_MASTER_HOSTNAME=\"$HOSTNAME\"+g" $S_USER_HOME/cryosparc3/cryosparc_master/config.sh
 sudo -u $S_USER $S_USER_HOME/cryosparc3/cryosparc_master/bin/cryosparcm start
 cd $S_USER_HOME/cryosparc3/cryosparc_worker
 sudo -u $S_USER ./bin/cryosparcw connect --master localhost --port 39000 --worker $HOSTNAME --nossd
